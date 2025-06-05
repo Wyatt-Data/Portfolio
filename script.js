4329dabcb721aa26be295edb946a52a37915e0e0
Wyatt-Data <wyattwilliams.data@gmail.com>
2025-06-05 14:09:12 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+const slides = document.querySelectorAll('.slide');
+let currentIndex = 0;
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.classList.toggle('active', i === index);
+  });
+}
+
+function nextSlide() {
+  currentIndex = (currentIndex + 1) % slides.length;
+  showSlide(currentIndex);
+}
+
+setInterval(nextSlide, 4000); // Change image every 4 seconds
